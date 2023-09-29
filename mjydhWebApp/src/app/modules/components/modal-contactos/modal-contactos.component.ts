27627c718e027259df819e1d1c8902cbed216fd9
dmosqueda <dmosqueda@santafe.gov.ar>
2023-09-29 12:38:01 -0300

    info sistemas

--- mjydhWebApp/src/app/modules/components/modal-contactos/modal-contactos.component.ts
+++ mjydhWebApp/src/app/modules/components/modal-contactos/modal-contactos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { ModalService } from '../../service/modal.service';
 import { SistemaService } from '../../service/sistema.service';
 
@@ -9,23 +9,34 @@ import { SistemaService } from '../../service/sistema.service';
 })
 export class ModalContactosComponent implements OnInit {
 
+  isDesktop : boolean
+
   sistema : string = ''
 
   contactos : any
+
   
   constructor(
-    protected modalSrv : ModalService,
-    private sistemaSrv : SistemaService
-    ){
-  
-  }
-  
-  ngOnInit(): void {
+      protected modalSrv : ModalService,
+      private sistemaSrv : SistemaService){
+      this.isDesktop = window.innerWidth >= 768 
+    }
 
+  @HostListener('window:resize', ['$event'])
+
+  onResize(event: any): void {
+    this.isDesktop = window.innerWidth >= 768
+  }
+
+  ngOnInit(): void {
+     
     this.sistemaSrv.data$.subscribe(contactos => {
-      this.contactos = contactos.contacto
-      console.log(this.contactos)
+      if (contactos) {
+        this.contactos = contactos.contacto
+        console.log(this.contactos)
+      }
     })
+    
 
   }
   
